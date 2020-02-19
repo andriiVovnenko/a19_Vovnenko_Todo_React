f989b091db29a7c72f5004e2e5c87a557bc2a5f5
Andrii <kabanvin@gmail.com>
2020-02-19 20:22:24 +0200

    filter by state

--- src/selectors/tasks.js
+++ src/selectors/tasks.js
@@ -1,28 +1,22 @@
 import sortDone from '../helpers/sortDone';
-import { SHOW_ACTIVE, SHOW_ALL } from './../constants/filterConstants'
+import { SHOW_ACTIVE, SHOW_ALL, SHOW_DONE } from './../constants/filterConstants'
 
 export const selectTasks = (state) => state.items;
-export const selectFilter = (state) => state.filter;
-export const selectUserInput = (state) => state.userInput;
 
 export const selectSortedTasks = (state) => {
     return Object.values(selectTasks(state)).sort(sortDone);
 };
 
-export const selectFilteredSortedTasks = (tasks, string) => {
-    if (!string) return state;
-    return tasks.filter(task => task.task.toLowerCase().startsWith(string.toString().toLowerCase()));
+export const selectFilteredSortedTasks = (state, string) => {
+    return state.filter(task => task.task.toLowerCase().startsWith(string.toString().toLowerCase()));
 };
 
-export const selectByDoneFilter = (state) => {
-    const tasks = selectSortedTasks(state);
-    const filter = selectFilter(state);
-    if (typeof filter === 'undefined') return tasks;
-    return tasks.filter(task => task.done === filter);
-
+export const selectByFilter = (state, filer) => {
+    if(filer === SHOW_ALL){
+        return state;
+    } else if(filer === SHOW_ACTIVE){
+        return state.filter(task => task.done);
+    } else {
+        return state.filter(task => !task.task.done);
+    }
 };
-
-export const selectByFilter = (state) => selectFilteredSortedTasks(
-    selectByDoneFilter(state),
-    selectUserInput(state),
-);
